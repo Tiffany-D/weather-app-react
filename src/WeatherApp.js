25f543092bbf22cd66ac7ba4098ce13efc86e422
Tiffany Dessouroux <tiffany.dessouroux@gmail.com>
2021-04-10 15:56:55 +0100

    corrections

--- src/WeatherApp.js
+++ src/WeatherApp.js
@@ -1,41 +1,42 @@
 import './App.css';
+import React from "react";
 
 export default function WeatherApp() {
   return (
-    <div className="Weather"
-      <form>
-      <div className="row"
-        <div className="col-9"
+    <div className="Weather">
+    <form>
+      <div className="row">
+        <div className="col-9">
       <input type="search"
         placeholder="Type a city..."
         className="form-control"/>
       </div>
-    <div className="col-3"
+    <div className="col-3">
       <input 
       type="submit"
       value="Search"
       className="btn btn-primary"/>
     </div >
       </div >
-      </form >
+       </form> 
         <h1>London</h1>
   <ul>
         <li>Saturday 15:20</li>
         <li>Description: Sunny</li>
   </ul>
-  <div className="row"
-        <div className="col-6"
+  <div className="row">
+        <div className="col-6">
       <img src="http://icons.iconarchive.com/icons/papirus-team/papirus-apps/512/weather-icon.png"/>
   14°C
-  </div >
-    <div className="col-6"
+</div>
+    <div className="col-6">
     <ul>
     <li> Humidity: 30%</li>
     <li>Wind: 7km/h</li>
     </ul>
   </div >
     </div >
-      </div>
+     </div> 
   );
 }
 
