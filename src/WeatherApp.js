9953a8224011c5dd6040da3f9d0c5b5bb4e3ae28
Tiffany Dessouroux <tiffany.dessouroux@gmail.com>
2021-04-23 12:38:59 +0100

    added location icon

--- src/WeatherApp.js
+++ src/WeatherApp.js
@@ -80,7 +80,9 @@ export default function WeatherApp(props) {
           
       <h1>
             
-            <i class="bi bi-geo-alt-fill">{" "}{weatherData.city}</i>
+           <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
+  <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/>
+</svg>{" "}{weatherData.city}
         </h1>
         
             <DateAndTime date={weatherData.date} />
